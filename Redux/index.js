15d20a8e8ff512a3db6f55d9fad02a91a758c6f5
ahmedfayezzz <ahmedfayezmine@gmail.com>
2020-10-23 12:48:04 +0200

    First pseudo code

--- Redux/index.js
+++ Redux/index.js
@@ -1,1 +1,42 @@
-console.log('from index js')
+const redux=require('redux')
+const createStore=redux.createStore
+
+const BUY_CAKE='BUY_CAKE'
+buyCack=()=>{
+  return(
+    {
+      type:BUY_CAKE,
+      info: 'first redux action'
+    }
+  )
+}
+
+const initialState={
+  numOfCackes:10
+}
+
+const reducer = (state=initialState,action)=>{
+  switch(action.type){
+    case BUY_CAKE:
+      return{
+        ...state,
+          numOfCackes:state.numOfCackes-1
+      }
+    default:
+      return state
+  }
+}
+
+const store = createStore(reducer)
+console.log('Initial state:', store.getState())
+//here we subscribe to the store so that a function 
+//would be called each time an action is dispatched to the store.
+//returned function from store.subscribe unsubscribes when called
+const unsubscibe = store.subscribe(()=>console.log('updated state:', store.getState()))
+
+store.dispatch(buyCack())
+store.dispatch(buyCack())
+store.dispatch(buyCack())
+
+unsubscibe()
+
